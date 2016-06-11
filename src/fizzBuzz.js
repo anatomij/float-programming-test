23a2f80608878228760228d2b5b5a11547e3098b
anatomij <artjom.svjatickis@gmail.com>
2016-06-10 20:10:32 -0700

    Solve fizzBuzz

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -17,5 +17,15 @@
  * 
  */
 module.exports = ( length ) => {
-
+  var result = [];
+  for(var i = 1; i < length + 1; i++) {
+    if(i % 3 === 0) {
+      result.push('fizz');
+    } else if(i % 5 === 0) {
+      result.push('buzz');
+    } else {
+      result.push(i);
+    }
+  }
+  return result;
 };
